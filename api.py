4d7dc4695640778f7ea086ee57557926089fa67b
ivanignashin <ivanignashin@mail.ru>
2024-09-10 22:36:59 +0300

    second

--- api.py
+++ api.py
@@ -5,11 +5,46 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class New(BaseModel):
-    name: str
-    age: int
+class Article(BaseModel):
+    head: str
+    text: str
+    author: str
 
 
-@app.post("/hello/")
-async def add(res: New):
-    return {"message": res.name}
+@app.post("/articles/add")
+async def add_article(article: Article):
+    import sqlite3
+    import dotenv
+    import os
+
+    dotenv.load_dotenv()
+    BAZA = os.getenv('BAZA')
+    conn = sqlite3.connect(BAZA)
+    cur = conn.cursor()
+    print(BAZA)
+    cur.execute('CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY AUTOINCREMENT, head TEXT, txt TEXT, author TEXT)')
+    conn.commit()
+    cur.execute('INSERT INTO articles (head, txt, author) VALUES (?, ?, ?)', (article.head, article.text, article.author))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {'status': 'ok'}
+
+
+@app.get('/articles/getall')
+async def get_articles() -> list[Article]:
+    import sqlite3
+    import dotenv
+    import os
+
+    dotenv.load_dotenv()
+    BAZA = os.getenv('BAZA')
+    conn = sqlite3.connect(BAZA)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM articles')
+    articles = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return [Article(head=i[1], text=i[2], author=i[3]) for i in articles]
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telebot import *
 import sqlite3
 import dotenv
 import os
-from api import add
+import requests
 
 
 dotenv.load_dotenv()
@@ -10,15 +10,63 @@ TOKEN = os.getenv('BOT_TOKEN')
 bot = TeleBot(token=TOKEN)
 
 
+BAZA = os.getenv('BAZA')
+conn = sqlite3.connect(BAZA)
+cur = conn.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY AUTOINCREMENT, head TEXT, txt TEXT, author TEXT)')
+conn.commit()
+cur.close()
+conn.close()
+
+
+article = {}
+
+button1 = types.KeyboardButton(text='Создать статью')
+button2 = types.KeyboardButton(text='Получить все статьи')
+welcome_keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, row_width=2, resize_keyboard=True, ).add(button1, button2)
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, text='Старт')
+    global welcome_keyboard
+    bot.send_message(message.chat.id, text='Это бот для создания статей.', reply_markup=welcome_keyboard)
 
 
+@bot.message_handler(commands=['newarticle'])
 @bot.message_handler(func=lambda message: message.text in ['i', 'Создать статью'])
 def api(message):
-    add()
+    sent = bot.send_message(message.chat.id, text='Введите заголовок статьи')
+    bot.register_next_step_handler(sent, article_text)
 
 
+def article_text(message):
+    article['head'] = message.text
+    sent = bot.send_message(message.chat.id, text='Введите текст статьи')
+    bot.register_next_step_handler(sent, article_commit)
+
+def article_commit(message):
+    global welcome_keyboard
+    article['text'] = message.text
+    article['author'] = message.from_user.first_name
+    response = requests.post('http://127.0.0.1:8000/articles/add', json=article)
+    if response.status_code == 200:
+        bot.send_message(message.chat.id, text='Статья создана', reply_markup=welcome_keyboard)
+    else:
+        bot.send_message(message.chat.id, text=f'Ошибка при добавлении статьи: {response.status_code} {response.text}', reply_markup=welcome_keyboard)
+        print("Ошибка при добавлении статьи:", response.status_code, response.text)
+
+
+@bot.message_handler(commands=['allarticles'])
+@bot.message_handler(func=lambda message: message.text in ['a', 'Получить все статьи'])
+def get_articles(message):
+    global welcome_keyboard
+    response = requests.get('http://127.0.0.1:8000/articles/getall')
+    if response.status_code == 200:
+        response = response.json()
+        for i in response:
+            bot.send_message(message.chat.id, text=f'{i["head"]} \n\n{i["text"]} \n\nАвтор: {i["author"]}')
+        bot.send_message(message.chat.id, text='Выберите действие', reply_markup=welcome_keyboard)
+    else:
+        bot.send_message(message.chat.id, text='Ошибка при получении данных.', reply_markup=welcome_keyboard)
+        print('Ошибка при получении данных.', response.status_code, response.text)
 
 bot.polling()
